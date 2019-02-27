ff962bcdfa8903bc70945747eebb18aefda1f232
Hayk Talalyan <talalyanhayk@gmail.com>
2019-02-27 15:23:19 +0400

    Added first Program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MyClass
+{
+    public string field;
+
+    public void Method()
+    {
+        Console.WriteLine(field);
+    }
+}
+
+class Program
+{
+  static void Main()
+  {
+        MyClass instance = new MyClass();
+        instance.field = "Hello World!";
+        Console.WriteLine(instance.field);
+        instance.Method();
+        Console.ReadKey();
+  }
+}
